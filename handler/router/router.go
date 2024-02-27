894b70236366bb4a1137a3abb38cb33c5dc7c65d
kappy <shangenjunfu9@gmail.com>
2024-02-27 16:34:38 +0900

    5

--- handler/router/router.go
+++ handler/router/router.go
@@ -3,10 +3,13 @@ package router
 import (
 	"database/sql"
 	"net/http"
+
+	"github.com/TechBowl-japan/go-stations/handler"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	// register routes
 	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", handler.NewHealthzHandler().ServeHTTP) // 第1引数にエンドポイント, 第2引数にhandlerを登録
 	return mux
 }
