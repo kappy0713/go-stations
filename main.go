0b22505f8867197bb1da814b1facabd15ff6df92
kappy <shangenjunfu9@gmail.com>
2024-04-09 09:44:39 +0900

    station6 finish!!

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"sync"
 	"time"
 
 	"github.com/TechBowl-japan/go-stations/db"
@@ -51,7 +54,35 @@ func realMain() error {
 	// NOTE: 新しいエンドポイントの登録はrouter.NewRouterの内部で行うようにする
 	mux := router.NewRouter(todoDB)
 
-	// TODO: サーバーをlistenする
-	http.ListenAndServe(port, mux)
+	server := &http.Server{ // httpServerの作成
+		Addr:    port, // デフォは:8080
+		Handler: mux,
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill) // 割り込みと強制終了のシグナル
+	defer stop()
+
+	wg := &sync.WaitGroup{} // goroutineの終了を待つ
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done() // goroutineが完了したらwgを1減らす(deferの処理は下から上)
+		<-ctx.Done()    // ctxがキャンセルされるまで以下の処理は実行されないようにする(中断処理信号が受信"されたら"サーバーをシャットダウン)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		err := server.Shutdown(ctx)
+		if err != nil {
+			log.Println("graceful shutdown miss", err)
+
+		} else {
+			log.Println("graceful shutdown")
+		}
+	}()
+
+	err = server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		return err
+	}
+	wg.Wait()
 	return nil
 }
